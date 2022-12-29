759291928ef1682648a2deebddf1c63e7641a8d9
MIINII <ft.dawn.o@gmail.com>
2022-12-29 10:33:06 +0900

    기본연산자 학습

--- client/chapter/core/06.typeConversion.js
+++ client/chapter/core/06.typeConversion.js
@@ -44,3 +44,20 @@ console.log(parseFloat(width, 10)); // parseFloat : 부동소수점까지..
 // 위에 나열한 것 이외의 것들
 console.log(Boolean('0'));
 console.log(!!null);
+
+console.log('' + 1 + 0); // '10'
+console.log('' - 1 + 0); // -10
+console.log(true + false); // 1
+console.log(6 / '3'); // 2
+console.log('2' * '3'); // 6
+console.log(4 + 5 + 'px'); // 9px
+console.log('$' + 4 + 5); //$45
+console.log('4' - 2); // 2
+console.log('4px' - 2); // NaN
+console.log(7 / 0); // Infinity
+console.log('  -9  ' + 5); // '   -95'
+console.log('  -9  ' - 5); // -14
+console.log(null + 1); // 1
+console.log(undefined + 1); // NaN
+console.log(' \t \n' - 2); // -2
+// \t 들여쓰기 \n 줄바꿈
