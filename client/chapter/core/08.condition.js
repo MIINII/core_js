a50e56b5eeb5950495fde9f74a530dcf67def378
MIINII <ft.dawn.o@gmail.com>
2022-12-29 13:15:35 +0900

    조건문 & 삼항연산자

---
+++ client/chapter/core/08.condition.js
@@ -0,0 +1,41 @@
+/* ---------------------------------------------------------------------- */
+/* Condition                                                              */
+/* ---------------------------------------------------------------------- */
+let number = prompt('숫자를 입력하쇼');
+
+let message = number > 0 ? 1 : number < 0 ? -1 : number == 0 ? 0 : '숫자여?';
+alert(message);
+
+// 그 영화 봤니?
+//     ↓
+// Yes | No
+//     | 영화 볼거니?
+//           ↓
+//       Yes | No
+
+// 영화 봤니?
+let didWatchMovie = 'yes';
+
+// 영화 볼거니?
+let goingToWatchMovie = 'no';
+
+if (didWatchMovie.includes('yes')) {
+  // if 문(statement)
+  console.log('재밋냐');
+  // else if 복수 조건 처리
+} else if (goingToWatchMovie == 'yes') {
+  console.logt('같이가잉');
+} else {
+  // else 절(caluse)
+  console.log('시러');
+}
+
+let movieMessage = didWatchMovie.includes('yes')
+  ? '재미있더라'
+  : goingToWatchMovie === 'yes'
+  ? '같이 보러가잉'
+  : '싫어잉';
+
+// 조건부 연산자
+
+// 멀티 조건부 연산자 식
